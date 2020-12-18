bff370454a9aaf018f872aabe070a8f032818ff6
RasmusDamgaard <xsz511@alumni.ku.dk>
2020-12-18 13:38:12 +0100

    adding a test

--- tests/webserver_tests.py
+++ tests/webserver_tests.py
@@ -11,46 +11,60 @@ def checkValidResponse(r):
     assert (date - datetime.datetime.utcnow()).total_seconds() < 5
 
 # Test an index of main folder is returned
-def test_valid_get_nopath_requestslib():
+def test_valid_get_nopath():
     r = requests.get('http://localhost:64321/')
     checkValidResponse(r)
     assert r.status_code == 200
     assert "<a href=" in r.text # encoded as links
     assert "folder" in r.text
     assert "webserver_tests.py" in r.text
-    print("Succeded: test_valid_get_nopath_requestslib")
+    print("Succeded: test_valid_get_nopath")
     
 # Test an index of subfolder is returned
-def test_valid_get_subfolder_requestslib():
+def test_valid_get_subfolder():
     r = requests.get('http://localhost:64321/folder1/')
     checkValidResponse(r)
     print(r.status_code)
     assert r.status_code == 200
     assert "<a href=" in r.text # encoded as links
     assert "testfile.txt" in r.text
-    print("Succeded: test_valid_get_subfolder_requestslib")
+    print("Succeded: test_valid_get_subfolder")
 
 # Test a file in subfolder
-def test_valid_get_ressource_requestslib():
+def test_valid_get_ressource():
     r = requests.get('http://localhost:64321/folder1/testfile.txt')
     checkValidResponse(r)
     assert r.status_code == 200
     assert "This is a test" == r.text 
+    print("Succeded: test_valid_get_ressource")
+
 
 # Test with invalid ressource path
-def test_invalid_patch_requestslib():
+def test_invalid_get():
     r = requests.patch('http://localhost:64321/filethatdoesnotexist')
     checkValidResponse(r)
     assert r.status_code == 404 # wrong file
+    print("Succeded: test_invalid_get")
+
+# Test with non-implemented
+def test_nonimplemented_patch():
+    r = requests.patch('http://localhost:64321/')
+    checkValidResponse(r)
+    assert r.status_code == 501 # wrong file
+    print("Succeded: test_invalid_patch_requestslib")
+
 
 # spawn web server for all tests
 server = subprocess.Popen(["python3", "../Code/webserver.py"])
 import time
 time.sleep(1)
 try:
-    test_valid_get_nopath_requestslib()
-    test_valid_get_subfolder_requestslib()
-    test_valid_get_ressource_requestslib()
-    test_invalid_patch_requestslib()
+    import os
+    os.system("curl -v http://localhost:64321")
+    test_valid_get_nopath()
+    test_valid_get_subfolder()
+    test_valid_get_ressource()
+    test_invalid_get()
+    test_nonimplemented_patch()
 finally:
     server.terminate()
